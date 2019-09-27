242d4f0ad5218c03103bf63ca93958d165c826dc
Daniel Vinther Wolf <danielvinther@youmail.dk>
2019-09-27 21:54:13 +0200

    Initial Commit. adds encoder.py and test_encoder

---
+++ src/encoder.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 26 20:00:32 2019
+
+@author: danie
+"""
+
+def encode(mess):
+    ''' Run Length Encoding - konverter strenge
+        fra 'wwwwwwwbbbb' '6w4b' '''
+    res = []
+    old = mess[0]
+    i = 0
+    for c in mess:
+        if c == old:
+            i += 1
+        else:
+            res.append('%d%c' % (i,c))
+            old = c
+            i = 1
+    res.append('%d%c' % (i,c))
+    return ''.join(res)
+
+def decoder(mess):
+    return ''
---
+++ test/test_encoder.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 26 19:58:54 2019
+
+@author: danie
+"""
+
+import pytest
+
+from src import encoder
+
+pytest
+
+def testEncoder():
+    assert encoder.encode('aa') == '2a'
+    assert encoder.encode('eeee') == '4e'
+    
